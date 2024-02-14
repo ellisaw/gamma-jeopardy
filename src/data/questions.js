56acf70dc0ac80fdc821dd7afcabfcabb09245be
Ellis Asadi-Wright <e.wright097@gmail.com>
2024-02-13 19:28:28 -0500

    Hide cell dollar amounts on click

---
+++ src/data/questions.js
@@ -0,0 +1,154 @@
+const questions = [
+    {
+        category: 'category-1',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+    {
+        category: 'category-2',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+    {
+        category: 'category-3',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+    {
+        category: 'category-4',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+    {
+        category: 'category-5',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+    {
+        category: 'category-6',
+        questions: [
+            {
+                question: 'question-1',
+                answer: 'answer-1'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-2'
+            },
+            {
+                question: 'question-2',
+                answer: 'answer-3'
+            },
+            {
+                question: 'question-4',
+                answer: 'answer-4'
+            },
+            {
+                question: 'question-5',
+                answer: 'answer-5'
+            },
+        ],
+    },
+];
+
+export default questions;
